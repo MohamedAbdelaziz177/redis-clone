7d2eb6772fde87d0e9640f0ae80c45dd6ddb5f80
MohamedAbdelaziz177 <mgouda@std.mans.edu.eg>
2026-04-14 17:24:26 +0200

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -35,21 +35,48 @@ func main() {
 	rd := bufio.NewReader(conn)
 
 	for {
-		rd.ReadByte()
-		str, err := rd.ReadString('\n')
 
-		if err != nil {
-			fmt.Println("Error reading string: ", err.Error())
-			return
-		}
-
-		str = strings.TrimRight(str, "\r\n")
+		str := parseArray(conn)[0]
 		if str == "PING" {
 			conn.Write([]byte("+PONG\r\n"))
 		}
 		//handleConnection(conn)
 	}
 
+}
+
+func parseArray(conn net.Conn) []string {
+
+	str, err := bufio.NewReader(conn).ReadString('\n')
+
+	if err != nil {
+		fmt.Println("Error reading string: ", err.Error())
+		return nil
+	}
+
+	str = strings.TrimRight(str, "\r\n")
+	str = strings.TrimLeft(str, "*")
+
+	count := 0
+	fmt.Sscanf(str, "%d", &count)
+
+	result := make([]string, count)
+
+	for i := 0; i < count; i++ {
+		str, _ = bufio.NewReader(os.Stdin).ReadString('\n')
+		str = strings.TrimRight(str, "\r\n")
+		str = strings.TrimLeft(str, "$")
+
+		var length int
+		fmt.Sscanf(str, "%d", &length)
+
+		str, _ = bufio.NewReader(os.Stdin).ReadString('\n')
+		str = strings.TrimRight(str, "\r\n")
+
+		result[i] = str
+	}
+
+	return result
 }
 
 // +PING\r\n
