d4bb984d6a1bb2eab8665c6849088e57896fc6d3
MohamedAbdelaziz177 <mgouda@std.mans.edu.eg>
2026-04-17 18:43:18 +0200

    Refactor RESP encoding functions to use constant for CRLF

--- app/resp/resp-encoder.go
+++ app/resp/resp-encoder.go
@@ -4,27 +4,34 @@ import (
 	"strconv"
 )
 
+const (
+	CRLF = "\r\n"
+)
+
 func EncodeString(value string) []byte {
-	return []byte("+" + value + "\r\n")
+	return []byte("+" + value + CRLF)
 }
 
 func EncodeError(value string) []byte {
-	return []byte("-" + value + "\r\n")
+	return []byte("-" + value + CRLF)
 }
 
 func EncodeInteger(value int) []byte {
-	return []byte(":" + strconv.Itoa(value) + "\r\n")
+	return []byte(":" + strconv.Itoa(value) + CRLF)
 }
 
 func EncodeBulk(value string) []byte {
-	return []byte("$" + strconv.Itoa(len(value)) + "\r\n" + value + "\r\n")
+	return []byte("$" + strconv.Itoa(len(value)) + CRLF + value + CRLF)
 }
 
 func EncodeArray(values []string) []byte {
+
 	var result []byte
-	result = append(result, []byte("*"+strconv.Itoa(len(values))+"\r\n")...)
+	result = append(result, []byte("*"+strconv.Itoa(len(values))+CRLF)...)
+
 	for _, val := range values {
 		result = append(result, EncodeBulk(val)...)
 	}
+
 	return result
 }
