56e0349c87ab2be79b066adc7321546bba68dd3c
MohamedAbdelaziz177 <mgouda@std.mans.edu.eg>
2026-04-17 19:20:14 +0200

    Implement lpush method for ListStore to prepend elements to a list

--- app/commands/list.go
+++ app/commands/list.go
@@ -48,6 +48,35 @@ func (ls *ListStore) rpush(value *resp.Value) []byte {
 	return []byte("-ERR invalid command format\r\n")
 }
 
+func (ls *ListStore) lpush(value *resp.Value) []byte {
+
+	if value.Type == resp.ARRAY && len(value.Array) >= 3 {
+
+		var elements = make([]string, 0)
+
+		for _, arg := range value.Array[2:] {
+			elements = append([]string{arg.Bulk}, elements...)
+		}
+
+		listName := value.Array[1].Bulk
+
+		ls.mu.Lock()
+		defer ls.mu.Unlock()
+
+		list, ok := ls.lists[listName]
+		if !ok {
+			list = []string{}
+		}
+
+		list = append(elements, list...)
+		ls.lists[listName] = list
+
+		return resp.EncodeInteger(len(list))
+	}
+
+	return resp.EncodeError("Err Handling LPUSH! ")
+}
+
 func (ls *ListStore) lrange(value *resp.Value) []byte {
 	if value.Type == resp.ARRAY && len(value.Array) == 4 {
 
