349b9439c7e23826dab43645bd51bfe25f1c1bd0
MohamedAbdelaziz177 <mgouda@std.mans.edu.eg>
2026-04-17 18:10:22 +0200

    Implement ListStore and rpush method for managing lists

--- app/commands/list.go
+++ app/commands/list.go
@@ -1,1 +1,36 @@
 package commands
+
+import (
+	"fmt"
+	"sync"
+
+	"github.com/codecrafters-io/redis-starter-go/app/resp"
+)
+
+type ListStore struct {
+	lists map[string][]string
+	mu    *sync.RWMutex
+}
+
+func (ls *ListStore) rpush(value *resp.Value) []byte {
+
+	if value.Type == resp.ARRAY && len(value.Array) == 3 {
+
+		listName := value.Array[1].Bulk
+		element := value.Array[2].Bulk
+
+		ls.mu.Lock()
+		defer ls.mu.Unlock()
+
+		list, ok := ls.lists[listName]
+		if !ok {
+			list = []string{}
+		}
+		list = append(list, element)
+		ls.lists[listName] = list
+
+		return []byte(fmt.Sprintf(":%d\r\n", len(list)))
+	}
+
+	return []byte("-ERR invalid command format\r\n")
+}
